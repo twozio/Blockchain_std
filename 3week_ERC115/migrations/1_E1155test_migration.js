d710f5618c72d938269bb7ef83663bb76a837ca7
twozio <people0113@naver.com>
2023-07-24 17:32:47 +0900

    fix line

--- 3week_ERC115/migrations/1_E1155test_migration.js
+++ 3week_ERC115/migrations/1_E1155test_migration.js
@@ -2,5 +2,5 @@ const Ticket = artifacts.require("E1155test.sol");
 
 module.exports = function (deployer) {
   // deployment steps
-  deployer.deploy(E1155test);
+    deployer.deploy(E1155test);
 };
