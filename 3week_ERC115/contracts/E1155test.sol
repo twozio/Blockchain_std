a3f6acf2c2ef9029d367f3aa8bbb1dd3bf5de1f7
twozio <people0113@naver.com>
2023-07-25 18:26:25 +0900

    update E1155test

--- 3week_ERC115/contracts/E1155test.sol
+++ 3week_ERC115/contracts/E1155test.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract E1155test is ERC1155 {
-    constructor() ERC1155("Test") {
+    constructor() ERC1155("https://dev-internship.s3.ap-northeast-2.amazonaws.com/erc1155/{id}.json") {}
 
+    function mint_all() public {
+        for(uint i = 1; i < 8; i++){
+            _mint(msg.sender, i, 10**i, "");
+        }
+    }
+
+    function burn_all() public {
+        for(uint i = 1; i < 8; i++){
+            _burn(msg.sender, i, 10**i);
+        }
+    }
+
+    function transfer(address to) public {
+        _safeTransferFrom(msg.sender, to, 1, 1,"");
     }
 }
