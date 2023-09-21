03797a025985588f9af9a8e13345fe09a1002700
twozio <people0113@naver.com>
2023-09-21 18:03:11 +0900

    update functions

--- 3DStore/contracts/Copyright.sol
+++ 3DStore/contracts/Copyright.sol
@@ -7,49 +7,80 @@ import "./DownloadTicket.sol";
 
 contract Copyright is ERC721URIStorage {
     DTicket public Dt;
+    address public owner;
 
-    string product = "Empty";
+    string product = "";
+    string uri = "https://your_base_url/product/";          // It will be work like a baseURI.
     uint private copyrightIndex = 1;
-    mapping(uint => bool) private copyrightAvailable;
-    mapping(uint => uint[]) public copyrightTickets;
-    mapping(uint => address) private musicOwners;
+    mapping(uint => bool) private copyrightAvailable;       // Manage for copyright state.
+    mapping(uint => uint[]) public copyrightTickets;        // Copyright link to ticket for management.
 
-    constructor() ERC721("3DStore", product) {}
-
-    /**
-     * @dev This function must be called first, then the mintCopyright function.
-     */
-    function setProductNumber (string memory productNumber) public {
-        product = productNumber;
+    constructor() ERC721("3DStore", product) {
+        owner = msg.sender;
     }
 
-    function mintCopyright () public {
+    /**
+     * @dev If you want to add a product number, you must call this function first before calling the mintCopyright function.
+     */
+    function setProductNumber(string memory _newproduct) public {
+        product = _newproduct;
+    }
+    /**
+     * @dev After deploy, you need to call this function and set the DownloadTicket.sol address.
+     */
+    function setDtAddress(address _DtAddress) public {
+        Dt = DTicket(_DtAddress);
+    }
+    // You can change the copyright metadata URI.
+    function resetURI(string memory _URI) public {
+        uri = _URI;
+    }
+    // Copyright NFT minting.
+    function mintCopyright() public {
         copyrightAvailable[copyrightIndex] = true;
 
         _mint(msg.sender, copyrightIndex);
-        _setTokenURI(copyrightIndex, string(abi.encodePacked("https://your_base_url/product/", copyrightIndex)));      // Example
+        _setTokenURI(copyrightIndex, string(abi.encodePacked(uri, copyrightIndex)));
+        // Init to product number.
+        setProductNumber("");
 
         copyrightIndex++;
     }
-
-    function mintDownloadTicket(uint copyrightId, uint counter) public {
+    // Download ticket minting.
+    function mintDownloadTicket(uint copyrightId, uint counter, uint price) public {
         require(msg.sender == ownerOf(copyrightId), "Only Copyright owner can mint the tickets");
 
-        uint newTicketId = Dt.mintTicket(copyrightId, counter);
-        
+        uint newTicketId = Dt.mintTicket(msg.sender, copyrightId, counter, price);
+        // For management.
         copyrightTickets[copyrightId].push(newTicketId);
     }
-
+    // Buy download ticket.
     function buyTicket(uint ticketId) public payable {
+        uint copyrightId = Dt.getCopyrightId(ticketId);
+        // Check the ticket available.
+        require(isAvailable(copyrightId), "The corresponding Copyright NFT is not available");
 
+        uint ticketPrice = Dt.getTicketPrice(ticketId);
+        require(ticketPrice > 0, "Ticket does not exist");
+
+        require(msg.value >= ticketPrice, "Not enough ether sent");
+        // payment
+        address seller = Dt.ownerOf(ticketId);
+        payable(seller).transfer(ticketPrice);
+        // Transfer ticket NFT to customer
+        Dt.transferTicket(seller, msg.sender, ticketId);
     }
 
+    function downloadTicket(uint ticketId) public {
+        Dt.download(msg.sender, ticketId);
+    }
+    // Change copyright available. You can stop or start the sale.
     function setAvailable(uint copyrightId, bool available) public {
+        require(msg.sender == ownerOf(copyrightId), "Only the Copyright owner can set availability");
         copyrightAvailable[copyrightId] = available;
     }
-
+    // Check the ticket available.
     function isAvailable(uint copyrightId) public view returns (bool) {
         return copyrightAvailable[copyrightId];
     }
-
 }
--- 3DStore/contracts/DownloadTicket.sol
+++ 3DStore/contracts/DownloadTicket.sol
@@ -8,26 +8,55 @@ import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721UR
 contract DTicket is ERC721URIStorage {
     Copyright public Cp;
 
+    struct TicketInfo {
+        uint copyrightId;
+        uint downloadCounter;
+        uint price;
+    }
+    // Only Copyright.sol can call some functions.
     modifier onlyCopyrightContract() {
         require(msg.sender == address(Cp), "Only the Music contract can call this function");
         _;
     }
 
     uint private ticketIndex = 1;
-    mapping(uint => uint) public ticketToCopyright;
-    mapping(uint => uint) public downloadCounter;
+    mapping(uint => TicketInfo) public ticketInfos;
+    /**
+     * @dev You must deploy the copyright contract first.
+     */
+    constructor(address _cpAddress) ERC721("Download Ticket", "DT") {
+        Cp = Copyright(_cpAddress);
+    } 
+    // Get the information that copyright's ticket.
+    function getCopyrightId(uint ticketId) public view returns (uint) {
+        require(_exists(ticketId), "Ticket does not exist");
+        return ticketInfos[ticketId].copyrightId;
+    }
+    // Get ticket price.
+    function getTicketPrice(uint ticketId) public view returns (uint) {
+        return ticketInfos[ticketId].price;
+    }
+    // Transfer ticket to customer. This function can only be called by a copyright contract.
+    function transferTicket(address ticketOwner, address recipient, uint ticketId) public onlyCopyrightContract {
+        transferFrom(ticketOwner, recipient, ticketId);
+    }
+    // Download ticket minting. This function can only be called by a copyright contract.
+    function mintTicket(address recipient, uint copyrightId, uint counter, uint price) public onlyCopyrightContract returns (uint) {
+        ticketInfos[ticketIndex] = TicketInfo(copyrightId, counter, price);
 
-    constructor() ERC721("Download Ticket", "DT") {} 
+        _mint(recipient, ticketIndex);
+        _setTokenURI(ticketIndex, string(abi.encodePacked("https://dev-internship.s3.ap-northeast-2.amazonaws.com/Ticket/1.json", ticketIndex)));
 
+        ticketIndex++;
+        return ticketIndex - 1;
+    }
+    // Ticket use.
+    function download(address recipient, uint ticketId) public onlyCopyrightContract {
+        require(_exists(ticketId), "Ticket does not exist");
+        require(ownerOf(ticketId) == recipient, "Caller does not own a ticket");
+        require(ticketInfos[ticketId].downloadCounter > 0, "You don't have any download chance!");
 
-    function mintTicket(uint copyrightId, uint counter) public onlyCopyrightContract returns (uint) {
-        ticketToCopyright[ticketIndex] = copyrightId;
-        downloadCounter[ticketIndex] = counter;
-
-        _mint(msg.sender, ticketIndex);
-        _setTokenURI(ticketIndex, string(abi.encodePacked("https://your_base_url/tickets/", ticketIndex)));
-
-        return ticketIndex;
+        ticketInfos[ticketId].downloadCounter--;
     }
 
 }
